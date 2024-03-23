ee636a92a30663977d5aaf9597c941cb0ccd45c4
luiz <luizaugusto3b@gmail.com>
2024-03-23 14:56:20 +0100

    - testing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,18 @@
 package org.example
 
 import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer
-import org.openstreetmap.osmosis.core.domain.v0_6.EntityType
+import org.openstreetmap.osmosis.core.domain.v0_6.Node
 import org.openstreetmap.osmosis.core.task.v0_6.Sink
 import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader
 import java.io.File
+import java.util.concurrent.atomic.AtomicLong
 
 fun main() {
     val file = File("src/main/resources/pomorskie-latest.osm.pbf")
     val gdyniaCity = City("Gdynia")
+    val nodes = mutableMapOf<Long, Node>()
     val osmosisReader = PbfReader(file, 0)
+    val sum = AtomicLong(0)
     osmosisReader.setSink(object : Sink {
         override fun close() {
             println("close")
@@ -17,6 +20,7 @@ fun main() {
 
         override fun complete() {
             println("complete")
+            println(sum)
         }
 
         override fun initialize(p0: MutableMap<String, Any>?) {
@@ -24,23 +28,27 @@ fun main() {
         }
 
         override fun process(p0: EntityContainer?) {
-//            p0?.entity?.tags?.filter {
-//                it.key == "shop" && it.value == "supermarket"
-//            }?.forEach {
-//                println("${it.key}: ${it.value}")
-//            }
-            p0?.entity?.tags?.run {
 
-                if (gdyniaCity.has(p0.entity)) {
-                    if (p0.entity.tags.any { "addr:street".equals(it.key, true) }) {
-                        p0.entity.tags.forEach {
-                            println("${it.key}: ${it.value}")
-                        }
+            p0?.entity?.run {
+                if (this is Node) {
+                    nodes.put(this.id, this)?.run {
+                        println(this)
                     }
-                    println()
+                    sum.incrementAndGet()
                 }
-
             }
+//            p0?.entity?.tags?.run {
+//
+//                if (gdyniaCity.has(p0.entity)) {
+//                    if (p0.entity.tags.any { it.key == "shop" && it.value == "supermarket" }) {
+//                        p0.entity.tags.forEach {
+//                            println("${it.key}: ${it.value}")
+//                        }
+//                        println()
+//                    }
+//                }
+//
+//            }
         }
 
     })
