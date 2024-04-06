954998e8651977f66589cb87fa6ad8cc9a2bcefc
luizcosta <luizaugusto3b@gmail.com>
2024-04-06 19:18:21 +0200

    - renaming street name from name to street

--- src/main/kotlin/Address.kt
+++ src/main/kotlin/Address.kt
@@ -1,36 +1,36 @@
 import org.example.City
 
 interface Address {
-    val name: String
+    val street: String
     val number: String
     val city: City
 }
 
 data class BaseAddress(
-    override val name: String,
+    override val street: String,
     override val number: String,
     override val city: City
 ) : Address
 
 data class Supermarket(
-    override val name: String,
+    override val street: String,
     override val number: String,
     override val city: City,
-    val brandName: String
+    val name: String
 ) : Address
 
 fun createAddressIfNotBlank(
-    name: String?,
+    street: String?,
     number: String?,
     cityName: String?,
     supermarket: Boolean,
     supermarketBrandName: String?
 ): Address? {
-    if (listOf(name, number, cityName).any { it.isNullOrBlank() }) {
+    if (listOf(street, number, cityName).any { it.isNullOrBlank() }) {
         return null
     }
     if (supermarket) {
-        return Supermarket(name.orEmpty(), number.orEmpty(), City(cityName.orEmpty()), supermarketBrandName.orEmpty())
+        return Supermarket(street.orEmpty(), number.orEmpty(), City(cityName.orEmpty()), supermarketBrandName.orEmpty())
     }
-    return BaseAddress(name.orEmpty(), number.orEmpty(), City(cityName.orEmpty()))
+    return BaseAddress(street.orEmpty(), number.orEmpty(), City(cityName.orEmpty()))
 }
--- src/main/kotlin/AddressExtractor.kt
+++ src/main/kotlin/AddressExtractor.kt
@@ -2,21 +2,21 @@ import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer
 import org.openstreetmap.osmosis.core.domain.v0_6.Tag
 
 fun extractAddress(entityContainer: EntityContainer?): Address? {
-    var name: String? = null
+    var street: String? = null
     var number: String? = null
     var cityName: String? = null
     var supermarket = false
     var supermarketBrandName: String? = null
     getTags(entityContainer).forEach {
         when (it.key) {
-            "addr:street" -> name = it.value
+            "addr:street" -> street = it.value
             "addr:housenumber" -> number = it.value
             "addr:city" -> cityName = it.value
             "shop" -> supermarket = it.value == "supermarket"
             "name" -> supermarketBrandName = it.value
         }
     }
-    createAddressIfNotBlank(name, number, cityName, supermarket, supermarketBrandName)?.let {
+    createAddressIfNotBlank(street, number, cityName, supermarket, supermarketBrandName)?.let {
         return it
     }
     return null
