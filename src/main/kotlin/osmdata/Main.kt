d9bb86e27c106eb2146240a6dc2e25548f8f2dee
luizcosta <luizaugusto3b@gmail.com>
2024-08-24 14:47:05 +0200

    - separate the html check

--- src/main/kotlin/osmdata/Main.kt
+++ src/main/kotlin/osmdata/Main.kt
@@ -4,6 +4,7 @@ import org.geotools.data.DataUtilities
 import org.geotools.geojson.feature.FeatureJSON
 import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader
 import java.io.File
+import java.nio.file.Files
 
 fun main() {
     val file = File("src/main/resources/pomorskie-latest.osm.pbf")
@@ -11,6 +12,10 @@ fun main() {
     val addressSink = AddressSink()
     osmosisReader.setSink(addressSink)
     osmosisReader.run()
+    createCheckHtml(addressSink)
+}
+
+private fun createCheckHtml(addressSink: AddressSink) {
     val addresses = addressSink.addresses.filter {
         it.getAttribute("name") != null
     }
@@ -19,6 +24,7 @@ fun main() {
     val template = File("src/main/resources/map-template.html")
     val html = template.bufferedReader().readText()
     val htmlWithGeosjon = html.replace("<GEOJSON>", geojson)
-    val fileWithGeojson = File("src/main/resources/index.html")
+    val fileWithGeojson = Files.createTempFile("map", ".html").toFile()
     fileWithGeojson.writeText(htmlWithGeosjon)
+    println("file://${fileWithGeojson.absolutePath}")
 }
