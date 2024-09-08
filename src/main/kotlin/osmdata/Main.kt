8434e95d79a44e31ba36eaa09746c780bd69f91a
luizcosta <luizaugusto3b@gmail.com>
2024-09-08 09:26:28 +0200

    - saving address to database

--- src/main/kotlin/osmdata/Main.kt
+++ src/main/kotlin/osmdata/Main.kt
@@ -2,46 +2,45 @@ package osmdata
 
 import org.geotools.data.DataUtilities
 import org.geotools.geojson.feature.FeatureJSON
+import org.locationtech.jts.geom.Point
+import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader
 import java.io.File
 import java.nio.file.Files
-import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.ResultSet
-import java.sql.Statement
 
 
 fun main() {
-//    val file = File("src/main/resources/pomorskie-latest.osm.pbf")
-//    val osmosisReader = PbfReader(file, Runtime.getRuntime().availableProcessors())
-//    val addressSink = AddressSink()
-//    osmosisReader.setSink(addressSink)
-//    osmosisReader.run()
+    val file = File("src/main/resources/pomorskie-latest.osm.pbf")
+    val osmosisReader = PbfReader(file, Runtime.getRuntime().availableProcessors())
+    val addressSink = AddressSink()
+    osmosisReader.setSink(addressSink)
+    osmosisReader.run()
 
     val jdbcUrl = "jdbc:postgresql://localhost:5432/homesearch_db"
     val username = "admin"
     val password = "admin"
-
+    val sql = "INSERT INTO public.address(street, \"number\", city, location)  VALUES (?, ?, ?, ST_SetSRID(ST_Makepoint(?, ?), 4326));"
 
     // Register the PostgreSQL driver
     Class.forName("org.postgresql.Driver")
 
 
     // Connect to the database
-    val connection: Connection = DriverManager.getConnection(jdbcUrl, username, password)
-
-    val statement: Statement = connection.createStatement()
-
-    val resultSet: ResultSet = statement.executeQuery("select * from spatial_ref_sys")
-
-    while (resultSet.next()) {
-        val columnValue = resultSet.getString("srid")
-        println("Column Value: $columnValue")
+    DriverManager.getConnection(jdbcUrl, username, password).use { conn ->
+        addressSink.addresses.forEach {
+            val point = it.defaultGeometry
+            if (point is Point) {
+                val pstmt = conn.prepareStatement(sql)
+                pstmt.setString(1, it.getAttribute("street").toString())
+                pstmt.setString(2, it.getAttribute("number").toString())
+                pstmt.setString(3, it.getAttribute("city").toString())
+                pstmt.setDouble(4, point.x)
+                pstmt.setDouble(5, point.y)
+                pstmt.executeUpdate()
+            }
+        }
     }
 
-    // Perform desired database operations
-
-    // Close the connection
-    connection.close()
 
     //createCheckHtml(addressSink)
 }
