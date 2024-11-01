379ab0fd9eccd6199f73ba10406e3622ffe5aaf2
luizcosta <luizaugusto3b@gmail.com>
2024-11-01 16:19:41 +0100

    - removing wildcard import

--- addressservice/src/main/kotlin/com/homesearch/addressservice/Application.kt
+++ addressservice/src/main/kotlin/com/homesearch/addressservice/Application.kt
@@ -1,9 +1,10 @@
 package com.homesearch.addressservice
 
-import io.ktor.server.engine.*
+import io.ktor.server.engine.embeddedServer
 import io.ktor.server.http.content.staticResources
-import io.ktor.server.netty.*
-import io.ktor.server.routing.*
+import io.ktor.server.netty.Netty
+import io.ktor.server.routing.routing
+
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
